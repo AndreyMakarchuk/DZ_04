51ed60ed3fe97e5dbd3029764aeec24e62bc90a5
Andrey Makarchuk <medved886@gmail.com>
2022-09-03 19:36:28 +0300

    Четвертая домашка

---
+++ dz_proga02/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число 
+//и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+static int SumVal(int value)
+{
+    if (value < 10)
+        return value;
+    int dig = value % 10;
+    int nextVal = value / 10;
+    return dig + SumVal(nextVal);
+}
+int result = SumVal(number);
+Console.WriteLine($"{number} -> " + result);
---
+++ dz_proga03/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран.
+
+int size = 8;
+int[] array = new int[size];
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+}
+void PrintArray(int[] array)
+{
+    Console.Write($"Массив из {size} элементов -> [ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+}
